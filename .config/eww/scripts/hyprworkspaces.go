469d02f93a33684d47d68ada500f392b421ba0d2
Ching Pei Yang <badnam3o.0@gmail.com>
2022-10-06 02:04:51 +0200

    add eww config

---
+++ .config/eww/scripts/hyprworkspaces.go
@@ -0,0 +1,325 @@
+/*
+ * Hyprland Workspaces
+ * Listens on hyprland socket and writes workspace state when needed, in json format
+ * Example output:
+ *		[{ "index": 0, "state": 2}, { "index": 1, "state": 1}]
+ *
+ * key "state" meaning can be found in `workspaceState`
+ **/
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/exec"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+var gHyprlandSockPath string
+var gLogPath string
+
+const PERSISTENT_WORKSPACES = 6
+
+type workspaceState int
+
+const (
+	INACTIVE workspaceState = 0 // No windows in this workspace
+	ACTIVE                  = 1 // Has window in this workspace
+	FOCUSED                 = 2 // Workspace is focused
+)
+
+type hyprState struct {
+	// context
+	// activewindow  struct{ class, title string }
+	workspaces []workspaceState
+	// quakeOnScreen bool
+}
+
+type server struct {
+}
+
+type Server interface {
+	startServer()
+}
+
+type event struct {
+	name, arg string
+}
+type cmd string
+
+// hyprctl query json return structs
+type workspaceJson struct {
+	Id int `json:"id"`
+	// Name            string `json:"name"`
+	// Monitor         string `json:"monitor"`
+	// Windows         int    `json:"windows"`
+	// Hasfullscreen   bool   `json:"hasfullscreen"`
+	// Lastwindow      string `json:"lastwindow"`
+	// Lastwindowtitle string `json:"lastwindowtitle"`
+}
+
+type clientJson struct {
+	// Address   string `json:"address"`
+	// At        [2]int `json:"at"`
+	// Size      [2]int `json:"size"`
+	Workspace struct {
+		Id int `json:"id"`
+		// Name string `json:"name"`
+	} `json:"workspace"`
+	// Floating bool   `json:"floating"`
+	// Monitor  int    `json:"monitor"`
+	// Class    string `json:"class"`
+	// Title    string `json:"title"`
+	// Pid      int    `json:"pid"`
+}
+
+// (*hyprState).workspacesChanged should be called whenever the workspaceState is
+// changed
+//
+// @param focus int The 0-indexed workspace number to focus, negative indicates to use previously focused workspace
+func (hs *hyprState) workspacesChanged(focus int) {
+	occupiedWorkspaces, err := hyprqueryWorkspaces()
+	if err != nil {
+		log.Printf("Error qureying workspaces: %s", err)
+		return
+	}
+	owsID := make([]int, len(occupiedWorkspaces))
+	for i, ws := range occupiedWorkspaces {
+		owsID[i] = ws.Id
+	}
+
+	ws := make([]workspaceState, max(append(owsID, PERSISTENT_WORKSPACES)))
+
+	for _, ows := range occupiedWorkspaces {
+		if ows.Id > len(ws) {
+			ws = append(ws, make([]workspaceState, ows.Id-len(ws))...)
+		} else if ows.Id == -99 {
+			continue
+		}
+
+		ws[ows.Id-1] = ACTIVE
+	}
+
+	if focus < 0 {
+		focus = index(hs.workspaces, FOCUSED)
+		// TODO: handle no previous focus
+	}
+
+	ws[focus] = FOCUSED
+
+	if reflect.DeepEqual(ws, hs.workspaces) {
+		return
+	}
+
+	hs.workspaces = ws
+	printJson(ws)
+}
+
+func handleHyprEvents(conn net.Conn, evChan chan<- event) {
+	for {
+		var buf [1024]byte
+		n, err := conn.Read(buf[:])
+
+		if err != nil {
+			log.Printf("reading from socket: %s", err)
+		}
+
+		msgs := strings.Split(string(buf[:n]), "\n")
+
+		for _, msg := range msgs {
+			if msg == "" {
+				continue
+			}
+			sep := strings.Index(msg, ">>")
+			if sep == -1 {
+				log.Printf("WARN: skipping invalid message: %s", msg)
+				continue
+			}
+
+			ev := msg[:sep]
+			data := msg[sep+2:]
+
+			evChan <- event{ev, data}
+		}
+
+	}
+}
+
+func startServer() {
+	evChan := make(chan event, 100)
+	hs := hyprState{
+		workspaces: make([]workspaceState, PERSISTENT_WORKSPACES),
+	}
+
+	currws, err := hyprqueryActiveWindow()
+	if err != nil {
+		log.Printf("Error getting active window: %s", err)
+	} else if currws.Workspace.Id < 1 {
+		currws.Workspace.Id = 1
+	}
+	hs.workspacesChanged(currws.Workspace.Id - 1)
+
+	printJson(hs.workspaces)
+
+	conn := connect(gHyprlandSockPath)
+	defer conn.Close()
+
+	go handleHyprEvents(conn, evChan)
+
+	for { // TODO terminate
+		select {
+		case ev := <-evChan:
+
+			if strings.HasSuffix(ev.name, "workspace") || ev.name == "focusedmon" || ev.name == "closewindow" ||
+				ev.name == "movewindow" {
+			}
+
+			switch ev.name {
+			case "workspace":
+				if ws, err := strconv.Atoi(ev.arg); err == nil {
+					hs.workspacesChanged(ws - 1)
+				} else {
+					log.Printf("Error converting event arg to int: %s", err)
+					hs.workspacesChanged(-1)
+				}
+			case "focusedmon":
+			case "activewindow":
+			case "fullscreen":
+			case "monitorremoved":
+			case "monitoradded":
+			case "createworkspace", "destroyworkspace":
+				hs.workspacesChanged(-1)
+			case "activelayout":
+			case "openwindow", "closewindow", "movewindow":
+				hs.workspacesChanged(-1)
+
+			default:
+				log.Printf("ERROR: got unrecognized event: %s", ev.name)
+
+			}
+		}
+	}
+}
+
+func main() {
+	hyprSig := os.Getenv("HYPRLAND_INSTANCE_SIGNATURE")
+	if hyprSig == "" {
+		os.Stderr.Write([]byte("No Hyprland instance found! Exiting..."))
+		os.Exit(1)
+	}
+
+	gHyprlandSockPath = "/tmp/hypr/" + hyprSig + "/.socket2.sock"
+	gLogPath = "/tmp/hypr/" + hyprSig + "/hyprcmd.log"
+
+	// f, err := os.OpenFile(gLogPath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer f.Close()
+	// log.SetOutput(f)
+	log.Printf("logging started")
+
+	startServer()
+
+	// flag.Parse()
+	// cmd := flag.Args()
+	// sendRequest(strings.Join(cmd, " "))
+}
+
+// utils
+func hyprctl(args ...string) (err error) {
+	cmd := exec.Command("hyprctl", args...)
+	err = cmd.Run()
+	return err
+}
+
+func hyprqueryActiveWindow() (*clientJson, error) {
+	cmd := exec.Command("hyprctl", "-j", "activewindow")
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, err
+	}
+	if err := cmd.Start(); err != nil {
+		return nil, err
+	}
+
+	client := &clientJson{}
+
+	if err := json.NewDecoder(stdout).Decode(&client); err != nil {
+		return nil, err
+	}
+
+	if err := cmd.Wait(); err != nil {
+		return nil, err
+	}
+
+	return client, err
+}
+
+func hyprqueryWorkspaces() (res []workspaceJson, err error) {
+	cmd := exec.Command("hyprctl", "-j", "workspaces")
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, err
+	}
+	if err := cmd.Start(); err != nil {
+		return nil, err
+	}
+
+	ws := make([]workspaceJson, 0)
+
+	if err := json.NewDecoder(stdout).Decode(&ws); err != nil {
+		return nil, err
+	}
+
+	if err := cmd.Wait(); err != nil {
+		return nil, err
+	}
+
+	return ws, err
+}
+
+func connect(path string) net.Conn {
+	conn, err := net.Dial("unix", path)
+	if err != nil {
+		log.Panicf("connect to %s: %s", path, err)
+	}
+
+	return conn
+}
+
+func printJson(arr []workspaceState) {
+	var builder strings.Builder
+	builder.WriteString(fmt.Sprintf(`[{"index":%d, "state":%d}`, 0, arr[0]))
+	for i, state := range arr[1:] {
+		builder.WriteString(fmt.Sprintf(`, {"index":%d, "state":%d}`, i+1, state))
+	}
+	builder.WriteRune(']')
+
+	fmt.Println(builder.String())
+}
+
+func max(xs []int) int {
+	m := xs[0]
+	for _, x := range xs[1:] {
+		if x > m {
+			m = x
+		}
+	}
+
+	return m
+}
+
+func index(slice []workspaceState, needle workspaceState) int {
+	for i, x := range slice {
+		if x == needle {
+			return i
+		}
+	}
+	return -1
+}
